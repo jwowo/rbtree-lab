0a4fa851f72e8d7e00588d2793bb2d686dad6e0b
JongWoo Han <jongwoo0221@gmail.com>
2021-12-08 08:32:22 +0000

    feat: free unfreed memory

--- test/test-rbtree.c
+++ test/test-rbtree.c
@@ -314,11 +314,11 @@ int main(void) {
   test_init();
   test_insert_single(1024);
   test_find_single(512, 1024);
-  // test_erase_root(128);
-  // test_minmax_suite();
-  // test_to_array_suite();
+  test_erase_root(128);
+  test_minmax_suite();
+  test_to_array_suite();
   test_distinct_values();
   test_duplicate_values();
-  // test_multi_instance();
+  test_multi_instance();
   printf("Passed all tests!\n");
 }
